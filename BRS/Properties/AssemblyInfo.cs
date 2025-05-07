e29b46a2750f159fd3bd653a309f843553d0dc1b
Felix <felix.indrawan@delamibrands.com>
2025-05-07 13:21:36 +0700

    perbaikan assembly information

--- BRS/Properties/AssemblyInfo.cs
+++ BRS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DRS")]
+[assembly: AssemblyTitle("BRS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DRS")]
+[assembly: AssemblyProduct("BRS")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
